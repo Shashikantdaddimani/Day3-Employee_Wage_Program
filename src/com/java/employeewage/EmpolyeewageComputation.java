9841ec0d507fd1e753c8cb5f46840dd21a3151a2
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-22 10:37:07 +0530

    Added:Welcome message of Employee wage computation program

---
+++ src/com/java/employeewage/EmpolyeewageComputation.java
@@ -0,0 +1,10 @@
+package com.java.employeewage;
+
+public class EmpolyeewageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to the employee wage computation program");
+
+	}
+
+}
