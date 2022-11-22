5ca38240fc6e18fef11801b9688d8be9ed28a065
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-22 11:40:51 +0530

    Added:checking employee is present or absent program

--- src/com/java/employeewage/EmpolyeewageComputation.java
+++ src/com/java/employeewage/EmpolyeewageComputation.java
@@ -1,10 +1,24 @@
 package com.java.employeewage;
 
+import java.util.Random;
+
 public class EmpolyeewageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to the employee wage computation program");
+		
+		Random random = new Random();
+		int empCheck = random.nextInt(2);
+		if(empCheck == 1)
+		{
+			System.out.println("Employee is Present.");
+		}
+		else
+		{
+			System.out.println("Employee is Absent.");
+		}
 
 	}
+	}
 
-}
+
