915f303a0a4a28a5f2f41300d1067595ad61a974
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-30 16:44:28 +0530

    Added: Uc-7 logic for write the code to class method to compute employee wage

--- src/com/java/employeewage/EmployeeWageComputationMain.java
+++ src/com/java/employeewage/EmployeeWageComputationMain.java
@@ -1,16 +1,16 @@
 package com.java.employeewage;
 
-public class EmployeeWageComputationMain 
-{
+public class EmployeeWageComputationMain {
 	/**
 	 * program execution starts from main method
+	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("welcome to the employee wage computation program");
-		EmpolyeewageComputation ewc = new EmpolyeewageComputation();
+		EmpolyeewageComputation employeewagecomputation = new EmpolyeewageComputation();
 		EmpolyeewageComputation.ComputeEmpWage();
-		ewc.displayTotalSalary();
+		employeewagecomputation.displayTotalSalary();
 	}
 
 }
