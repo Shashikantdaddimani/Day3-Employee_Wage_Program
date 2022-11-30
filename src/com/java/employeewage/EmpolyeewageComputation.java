20219afc9ac35620596d960342e55ba62f2404eb
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-11-30 16:59:05 +0530

    Added:Uc-7 logic for write a code to class method to compute employee wage

--- src/com/java/employeewage/EmpolyeewageComputation.java
+++ src/com/java/employeewage/EmpolyeewageComputation.java
@@ -3,62 +3,63 @@
  *Employee Wage - Use Class Method and Class Variables
  */
 package com.java.employeewage;
+
 /**
  * imports all the class of the java.util package
  */
 import java.util.*;
 
-public class EmpolyeewageComputation 
-{
+public class EmpolyeewageComputation {
 	/*
-	 * Declaring And initialization of veriable 
+	 * Declaring And initialization of variable
 	 */
-		static int empWagePerHour = 20;
-		static int fullDayHour = 8;
-		static int halfDayHour = 4;
-		static int salary = 0;
-		static int totalSalary = 0;
-		static int totalWorkingDays = 20;
-		static int days =0;
-		static int totalWorkingHours = 0;
+	static int empWagePerHour = 20;
+	static int fullDayHour = 8;
+	static int halfDayHour = 4;
+	static int salary = 0;
+	static int totalSalary = 0;
+	static int totalWorkingDays = 20;
+	static int days = 0;
+	static int totalWorkingHours = 0;
+
+	/*
+	 * logic for checking empwage using the method
+	 */
+	static void ComputeEmpWage() {
 		/*
-		 * logic for checking empwage using the method
+		 * taking random object because Generating the random number
 		 */
-		static void ComputeEmpWage() 
-		{
-			/*
-			 * taking random object because Generating the random number
-			 */
 		Random random = new Random();
-		while(days != totalWorkingDays && totalWorkingHours != 100)
-		{
-			
+		while (days != totalWorkingDays && totalWorkingHours != 100) {
+
 			int empCheck = random.nextInt(3);
 			/*
-			 * logic for checking conditions using switch case statement 
+			 * logic for checking conditions using switch case statement
 			 */
-			switch(empCheck)
-			{
-				case 0: System.out.println("Employee is Absent.");
-						break;	
-				case 1: System.out.println("Employee is Present.");
-						salary = empWagePerHour * fullDayHour ;
-						totalWorkingHours = totalWorkingHours + fullDayHour;
-						break;
-				case 2: System.out.println("Present for Half Day");
-						salary = empWagePerHour * halfDayHour ;
-						totalWorkingHours = totalWorkingHours + halfDayHour;
+			switch (empCheck) {
+			case 0:
+				System.out.println("Employee is Absent.");
+				break;
+			case 1:
+				System.out.println("Employee is Present.");
+				salary = empWagePerHour * fullDayHour;
+				totalWorkingHours = totalWorkingHours + fullDayHour;
+				break;
+			case 2:
+				System.out.println("Present for Half Day");
+				salary = empWagePerHour * halfDayHour;
+				totalWorkingHours = totalWorkingHours + halfDayHour;
 			}
-				System.out.print("random: "+empCheck+"\t");
-				System.out.print("Working Hours: "+totalWorkingHours+"\t");
-				System.out.print("\tSalary is: "+salary+"\t");
-				totalSalary = totalSalary + salary;
-				days++;
+			System.out.print("random: " + empCheck + "\t");
+			System.out.print("Working Hours: " + totalWorkingHours + "\t");
+			System.out.print("\tSalary is: " + salary + "\t");
+			totalSalary = totalSalary + salary;
+			days++;
 		}
-		System.out.println("Total Salary: "+totalSalary);
+		System.out.println("Total Salary: " + totalSalary);
 	}
-		void displayTotalSalary() {
-			System.out.println("Total Salary: "+totalSalary);
-			}
-}
 
+	void displayTotalSalary() {
+		System.out.println("Total Salary: " + totalSalary);
+	}
+}
